10ac4f3485ac4fb804c7222835b8deb21a4dbe68
Aristagets Kafian <Aristageck@gmail.com>
2024-05-23 16:48:54 +0700

    add-servicenav

--- js/main.js
+++ js/main.js
@@ -1,20 +1,60 @@
 const El = document.getElementById('active-nav');
-function Nav1(){
-    El.style.left = "6px";
+const parent = document.getElementById('service-nav').offsetWidth;
+
+function moveNav(x, stop) {
+    if (x < stop) {
+        let i = x;
+        let interval = setInterval(() => {
+            El.style.left = i + "px";
+            i += 4;
+            if (i >= stop) {
+                clearInterval(interval);
+            }
+        }, 1);
+    } else {
+        let j = x;
+        let interval = setInterval(() => {
+            El.style.left = j + "px";
+            j -= 4;
+            if (j <= stop) {
+                clearInterval(interval);
+            }
+        }, 1);
+    }
 }
-function Nav2(){
-    let i = 6;
-    let interval = setInterval(() => {
-        El.style.left = i + "px";
-        i += 4;
-        if(i >= 347){
-            clearInterval(interval);
-        }
-    }, 1)
+
+
+const serviceTitle = document.getElementById('service-title');
+const serviceDesc = document.getElementById('service-description');
+const serviceImg = document.getElementById('service-img');
+
+function Nav1() {
+    serviceTitle.innerHTML = "Бизнес-анализ";
+    serviceImg.src = "img/service1.svg";
+    let x = El.offsetLeft;
+    moveNav(x, 4);
 }
-function Nav3(){
-    El.style.left = "50%";
+
+function Nav2() {
+    serviceTitle.innerHTML = "Backend";
+    serviceImg.src = "img/service2.svg";
+    let stop = (parent / 4) + 4;
+    let x = El.offsetLeft;
+    moveNav(x, stop);
 }
-function Nav4(){
-    El.style.left = "75%";
+
+function Nav3() {
+    serviceTitle.innerHTML = "Frontend";
+    serviceImg.src = "img/service3.svg";
+    let stop = (parent / 2) + 4;
+    let x = El.offsetLeft;
+    moveNav(x, stop);
+}
+
+function Nav4() {
+    serviceTitle.innerHTML = "Mobile";
+    serviceImg.src = "img/service4.svg";
+    let stop = parent - (parent / 4) - 4;
+    let x = El.offsetLeft;
+    moveNav(x, stop);
 }
