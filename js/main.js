6cbda5733742bd82d505d767d2759edacfea611e
Aristagets Kafian <Aristageck@gmail.com>
2024-06-13 11:25:16 +0700

    cosmetics

--- js/main.js
+++ js/main.js
@@ -2,12 +2,15 @@
 // Открыть список в footer
 function openFooter(str){
     const footBlock = document.getElementById(str);
+    const arrowFoot = document.querySelector('.' + str + ' .foot-arrow');
     if( window.innerWidth <= 767 ){
         if (footBlock.style.display !== "flex"){
         footBlock.style.display = "flex";
+        arrowFoot.src = 'img/up.svg';
         }
         else{
         footBlock.style.display = "none";
+        arrowFoot.src = 'img/down.svg';
         }
     } 
     else{
@@ -16,61 +19,69 @@ function openFooter(str){
 }
 
 
-function serviceBlock(serviceName, number, imgSrc) {
+
+function showService(serviceType) {
     const serviceElements = document.querySelectorAll('.service-nav-child'); 
     serviceElements.forEach(function(element) {
         element.classList.remove('active');
-    });
-    
-    let serviceTitle = document.querySelector('h3'); 
-    serviceTitle.textContent = serviceName; 
-    
-    let selectedElement = document.querySelectorAll('.service-nav-child')[number - 1];
+    }); 
+
+    let selectedElement = document.getElementById(serviceType + '_but');
     selectedElement.classList.add('active');
 
+    const serviceInfos = document.querySelectorAll('.service-info'); 
+    serviceInfos.forEach(function(info) {
+        info.style.display = 'none';
+    });
+    
+    let selectedInfo = document.getElementById('service-info-' + serviceType);
+    selectedInfo.style.display = 'block';
+
     let serviceImg = document.querySelector('.service-img');
-    serviceImg.src = 'img/' + imgSrc;
+    serviceImg.src = 'img/' + serviceType + '.svg';  
 }
+
+
+// function serviceBlock(serviceName, number, imgSrc) {
+//     const serviceElements = document.querySelectorAll('.service-nav-child'); 
+//     serviceElements.forEach(function(element) {
+//         element.classList.remove('active');
+//     });
+    
+//     let serviceTitle = document.querySelector('.service-info h3'); 
+//     serviceTitle.textContent = serviceName; 
+    
+//     let selectedElement = document.querySelectorAll('.service-nav-child')[number - 1];
+//     selectedElement.classList.add('active');
+
+//     let serviceImg = document.querySelector('.service-img');
+//     serviceImg.src = 'img/' + imgSrc;
+// }
 
 
 //скролл по якорным ссылкам
 function getScroll(blockName) {
-    function animate(draw, duration) {
-        let start = performance.now();
-        requestAnimationFrame(function animate(time) {
-            let timePassed = time - start;
-            if (timePassed > duration) {
-                timePassed = duration;
-            }
-            draw(timePassed);
-            if (timePassed < duration) {
-                requestAnimationFrame(animate);
+    let nav = document.getElementById(blockName);
+    let links = nav.getElementsByTagName('a');  
+
+    for (let i = 0; i < links.length; i++) {
+        links[i].addEventListener('click', (event) => {
+            event.preventDefault();
+            let linkAttr = links[i].getAttribute('href').match(/[a-z]/gi);
+            let linkStr = linkAttr.join('');
+            let div = document.getElementById(linkStr);
+            let divId = div.getAttribute('id');
+
+            if (linkStr == divId) {
+                window.scroll({
+                    top: div.getBoundingClientRect().top - 80,
+                    behavior: 'smooth'
+                });
             }
         });
     }
-    let nav = document.getElementById(blockName),
-    links = nav.getElementsByTagName('a');  
-    for (let i = 0; i < links.length; i++) {
-        links[i].addEventListener('click', () => {
-            // Отменяем стандартные действия браузера
-            event.preventDefault();
-            let link = links[i],
-                linkAttr = link.getAttribute('href');
-            linkAttr = linkAttr.match(/[a-z]/gi);
-            let linkStr = linkAttr.join(''),
-                div = document.getElementById(linkStr),
-                divId = div.getAttribute('id');
-            if (linkStr == divId) {
-                // let divIdY = div.getBoundingClientRect().top;
-                // window.scrollBy(0, divIdY / 20 -3);
-                animate(function(timePassed) {
-                    let divIdY = div.getBoundingClientRect().top - 80;
-                    window.scrollBy(0, divIdY / 15);
-                }, 1500);
-            }
-        });
-    };
 }
+
 getScroll('header_menu');
 getScroll('services');
 
