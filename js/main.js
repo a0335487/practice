1dd9ce6ad46f4a3ef4c42d5bcc7e1d9c100f474a
Aristagets Kafian <Aristageck@gmail.com>
2024-06-19 11:22:42 +0700

    remakeData

--- js/main.js
+++ js/main.js
@@ -23,48 +23,53 @@ for (let smoothLink of smoothLinks) {
 
 
 //JSON
+const scrollNav = document.querySelector('.scroll-nav');
 
-const serviceNavChildren = document.querySelectorAll('.service-nav-child');
-    
-serviceNavChildren.forEach(function(child) {
-    child.addEventListener('click', function(event) {
+fetch('https://gist.githubusercontent.com/a0335487/5073a29953069a00aae5b6e3e7e1ca9b/raw/e5fded489c03c8a24ef64431fac93008a7c70c57/data.json')
+  .then(response => response.json())
+  .then(data => {
+    for (let i = 0; i < data.items.length; i++) {
+      const serviceNavChild = document.createElement('div');
+      serviceNavChild.classList.add('service-nav-child');
+      const h5 = document.createElement('h5');
+      h5.innerText = data.items[i].title;
+      serviceNavChild.appendChild(h5);
+      scrollNav.appendChild(serviceNavChild);
+    }
+
+    const serviceNavChildren = document.querySelectorAll('.service-nav-child');
+    serviceNavChildren[0].classList.add('active');
+
+    document.getElementById('service-title').innerText = data.items[0].title;
+    document.getElementById('service-description').innerText = data.items[0].description;
+    document.getElementById('service-img').src = "img/" + data.items[0].img;
+
+    serviceNavChildren.forEach(function(child) {
+      child.addEventListener('click', function(event) {
         const serviceName = child.querySelector('h5').innerText;
         showService(event, serviceName);
+      });
     });
-
-    fetch('https://gist.githubusercontent.com/a0335487/5073a29953069a00aae5b6e3e7e1ca9b/raw/e5fded489c03c8a24ef64431fac93008a7c70c57/data.json')
-    .then(response => response.json())
-    .then(data => {
-        const navTitles = document.querySelectorAll('.scroll-nav h5');
-        navTitles.forEach(function(title, index) {
-            title.innerText = data.items[index].title;
-        });
-        document.getElementById('service-title').innerText =  data.items[0].title;
-        document.getElementById('service-description').innerText = data.items[0].description;
-        document.getElementById('service-img').src = "img/" + data.items[0].img;
-    })
-    .catch(error => console.error('Error:', error));
-});
+  })
+  .catch(error => console.error('Error:', error));
 
 function showService(event, serviceName) {
-    fetch('https://gist.githubusercontent.com/a0335487/5073a29953069a00aae5b6e3e7e1ca9b/raw/e5fded489c03c8a24ef64431fac93008a7c70c57/data.json')
+  fetch('https://gist.githubusercontent.com/a0335487/5073a29953069a00aae5b6e3e7e1ca9b/raw/e5fded489c03c8a24ef64431fac93008a7c70c57/data.json')
     .then(response => response.json())
     .then(data => {
-      
-        const serviceElements = document.querySelectorAll('.service-nav-child'); 
-        serviceElements.forEach(function(element) {
-            element.classList.remove('active');
-        }); 
-        
-        const service = data.items.find(item => item.title === serviceName);
-        if (service) {
-            document.getElementById('service-title').innerText = service.title;
-            document.getElementById('service-description').innerText = service.description;
-            document.getElementById('service-img').src = "img/" + service.img;
-            const parent = event.target.closest(".service-nav-child");
-            parent.classList.add('active');
-        }
+      const serviceElements = document.querySelectorAll('.service-nav-child');
+      serviceElements.forEach(function(element) {
+          element.classList.remove('active');
+      });
+
+      const service = data.items.find(item => item.title === serviceName);
+      if (service) {
+          document.getElementById('service-title').innerText = service.title;
+          document.getElementById('service-description').innerText = service.description;
+          document.getElementById('service-img').src = "img/" + service.img;
+          const parent = event.target.closest(".service-nav-child");
+          parent.classList.add('active');
+      }
     })
     .catch(error => console.error('Error:', error));
 }
-
