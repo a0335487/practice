70371a1c9a50cff14521312c982598cd8ef4ba7b
Aristagets Kafian <Aristageck@gmail.com>
2024-06-19 10:32:03 +0700

    remakeData

--- js/main.js
+++ js/main.js
@@ -7,30 +7,6 @@ function openFooter(event){
     }  
 }
 
-
-
-// function showService(event, serviceType) {
-//     const serviceElements = document.querySelectorAll('.service-nav-child'); 
-//     serviceElements.forEach(function(element) {
-//         element.classList.remove('active');
-//     }); 
-
-//     // const selectedElement = document.getElementById(serviceType + '_but');
-//     const parent = event.target.closest(".service-nav-child");
-//     parent.classList.add('active');
-
-//     const serviceInfos = document.querySelectorAll('.service-main'); 
-//     serviceInfos.forEach(function(info) {
-//         info.style.display = 'none';
-//     });
-    
-//     const selectedInfo = document.getElementById('service-info-' + serviceType);
-//     selectedInfo.style.display = 'block';
- 
-// }
-
-
-
 //скролл по якорным ссылкам
 const smoothLinks = document.querySelectorAll("a[href^='#']");
 for (let smoothLink of smoothLinks) {
@@ -46,38 +22,46 @@ for (let smoothLink of smoothLinks) {
 };
 
 
-
-
 //JSON
-fetch('https://gist.githubusercontent.com/a0335487/5073a29953069a00aae5b6e3e7e1ca9b/raw/e5fded489c03c8a24ef64431fac93008a7c70c57/data.json')
-.then(response => response.json())
-.then(data => {
-    const navTitles = document.querySelectorAll('.scroll-nav h5');
-    navTitles.forEach(function(title, index) {
-        title.innerText = data.items[index].title;
+
+const serviceNavChildren = document.querySelectorAll('.service-nav-child');
+    
+serviceNavChildren.forEach(function(child) {
+    child.addEventListener('click', function(event) {
+        const serviceName = child.querySelector('h5').innerText;
+        showService(event, serviceName);
     });
-})
-.catch(error => console.error('Error:', error));
+
+    fetch('https://gist.githubusercontent.com/a0335487/5073a29953069a00aae5b6e3e7e1ca9b/raw/e5fded489c03c8a24ef64431fac93008a7c70c57/data.json')
+    .then(response => response.json())
+    .then(data => {
+        const navTitles = document.querySelectorAll('.scroll-nav h5');
+        navTitles.forEach(function(title, index) {
+            title.innerText = data.items[index].title;
+        });
+    })
+    .catch(error => console.error('Error:', error));
+});
 
 function showService(event, serviceName) {
     fetch('https://gist.githubusercontent.com/a0335487/5073a29953069a00aae5b6e3e7e1ca9b/raw/e5fded489c03c8a24ef64431fac93008a7c70c57/data.json')
     .then(response => response.json())
     .then(data => {
-        const service = data.items.find(item => item.title === serviceName);
+      
+        const serviceElements = document.querySelectorAll('.service-nav-child'); 
+        serviceElements.forEach(function(element) {
+            element.classList.remove('active');
+        }); 
         
+        const service = data.items.find(item => item.title === serviceName);
         if (service) {
             document.getElementById('service-title').innerText = service.title;
             document.getElementById('service-description').innerText = service.description;
             document.getElementById('service-img').src = "img/" + service.img;
-
-            const serviceElements = document.querySelectorAll('.service-nav-child'); 
-            serviceElements.forEach(function(element) {
-                element.classList.remove('active');
-            });
-
             const parent = event.target.closest(".service-nav-child");
             parent.classList.add('active');
         }
     })
     .catch(error => console.error('Error:', error));
 }
+
